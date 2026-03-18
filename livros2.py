28de476548d4439253fdb29f1a5ba5f37e3b699b
Michael Silva <michael_araujo2012@hotmail.com>
2026-03-17 22:09:09 -0300

    add catalogo com logica para dicionario de livros antigos

--- livros2.py
+++ livros2.py
@@ -34,3 +34,42 @@ print(f"Catálogo {catalogo}")
 
 # import pprint (from pprint import pprint) - pretty print (impressão “bonita”)
 pprint(catalogo)
+
+# -------------------------------------------------------------------------------
+
+
+catalogo_impresso = {
+    "Dom Quixote": {"autor": "Miguel de Cervantes", "ano": 1605},
+    "Orgulho e Preconceito": {"autor": "Jane Austen", "ano": 1813},
+    "O Grande Gatsby": {"autor": "F. Scott Fitzgerald", "ano": 1925},
+    "Cem Anos de Solidão": {"autor": "Gabriel García Márquez", "ano": 1967},
+    "1984": {"autor": "George Orwell", "ano": 1949},
+    "Harry Potter e a Pedra Filosofal": {"autor": "J.K. Rowling", "ano": 1997},
+    "O Senhor dos Anéis": {"autor": "J.R.R. Tolkien", "ano": 1954},
+    "A Revolução dos Bichos": {"autor": "George Orwell", "ano": 1945},
+    "O Apanhador no Campo de Centeio": {"autor": "J.D. Salinger", "ano": 1951},
+    "O Código Da Vinci": {"autor": "Dan Brown", "ano": 2003},
+}
+
+livros_antigos = {}
+
+for titulo, info in catalogo_impresso.items():
+    ano = info["ano"]
+
+    if ano < 1950:
+        livros_antigos[titulo] = info
+
+print(f"\nLivros antigos: {livros_antigos}")
+
+
+
+
+# list comprehension
+livros_antigos = {
+    titulo: info
+    for titulo, info in catalogo_impresso.items()
+    if info["ano"] < 1950
+}
+
+print("\nLivros antigos")
+pprint(livros_antigos, sort_dicts=False)
