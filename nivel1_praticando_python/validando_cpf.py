80a78f9d7c4f210f8b99d6f6cfa58e5fd68e95a9
Michael Silva <michael_araujo2012@hotmail.com>
2026-09-08 19:27:35 -0300

    adicionando validação de quantidade de digitos

--- nivel1_praticando_python/validando_cpf.py
+++ nivel1_praticando_python/validando_cpf.py
@@ -1,15 +1,11 @@
-cpf =  input("Digite seu CFP: ")
+
+cpf =  input("Digite seu CPF: ")
 qnt_numeros = 11
 
-def validador(cpf, qnt_numeros):
-    cpf = str(cpf)
-    so_tem_numeros = cpf.isdigit()
-    tamanho_certo = len(cpf) == qnt_numeros
 
-    return so_tem_numeros and tamanho_certo
-
-
-if validador(cpf, qnt_numeros):
-    print("CPF válido")
+if cpf.isdigit() == False:
+    print("Erro: O CPF deve conter apenas números.")
+elif len(cpf) != qnt_numeros:
+    print("Erro: O CPF deve ter exatamente 11 dígitos.")
 else:
-    print("CPF inválido")
+    print("CPF válido.")
